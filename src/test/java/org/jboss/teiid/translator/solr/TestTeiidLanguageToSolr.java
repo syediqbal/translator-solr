3c4e53bfdc135486309c3d875e3a26fe34036c39
jmarley <jmarley@redhat.com>
2013-11-01 07:45:43 -0400

    changed metadata interface

--- src/test/java/org/jboss/teiid/translator/solr/TestTeiidLanguageToSolr.java
+++ src/test/java/org/jboss/teiid/translator/solr/TestTeiidLanguageToSolr.java
@@ -19,27 +19,26 @@ import org.teiid.query.unittest.RealMetadataFactory;
 import org.teiid.translator.TypeFacility;
 import org.teiid.translator.solr.execution.SolrSQLHierarchyVistor;
 
+@SuppressWarnings("nls")
 public class TestTeiidLanguageToSolr {
 
-/*	private QueryMetadataInterface solrMetadata() {
+	private QueryMetadataInterface solrMetadata(String ddl, String vdbName, String modelName) throws Exception {
 
-		MetadataFactory factory = new MetadataFactory("", 1, "", SystemMetadata
-				.getInstance().getRuntimeTypeMap(), new Properties(), "");
-		createFakeMetadata(factory);
-		return new TransformationMetadata(null, new CompositeMetadataStore(
-				factory.asMetadataStore()), null,
-				RealMetadataFactory.SFM.getSystemFunctions(), null);
-	}*/
+		MetadataFactory mf = new MetadataFactory("", 1, "", SystemMetadata.getInstance().getRuntimeTypeMap(), new Properties(), "");
+		createFakeMetadata(mf);
+		TransformationMetadata tm = RealMetadataFactory.fromDDL(ddl, vdbName, modelName);
+		return tm;
+	}
 
-	/*private void createFakeMetadata(MetadataFactory factory) {
+	private void createFakeMetadata(MetadataFactory mf) {
 		
-		 * Table person = factory.addTable("Person"); Column id =
-		 * factory.addColumn("id", TypeFacility.RUNTIME_NAMES.INTEGER, person);
-		 * Column name = factory.addColumn("name",
-		 * TypeFacility.RUNTIME_NAMES.STRING, person);
-		 Column age = factory.addColumn("age",
-				TypeFacility.RUNTIME_NAMES.INTEGER, person);
-	}*/
+		  Table example =mf.addTable("example"); Column id =
+		 mf.addColumn("price", TypeFacility.RUNTIME_NAMES.FLOAT, example);
+		  Column name =mf.addColumn("weight",
+		  TypeFacility.RUNTIME_NAMES.FLOAT, example);
+		 Column age =mf.addColumn("popularity",
+				TypeFacility.RUNTIME_NAMES.INTEGER, example);
+	}
 
 	
 	private void testTranslation(String sql, String expectedCQL){
@@ -47,31 +46,31 @@ public class TestTeiidLanguageToSolr {
 		
 		SolrSQLHierarchyVistor visitor = new SolrSQLHierarchyVistor(null, null);
 		visitor.visitNode(select);
-	/*	System.out.println(visitor.visit(select));
+//		System.out.println(visitor.visit(select));
 		//Assert.assertEquals(expectedCQL, visitor.getTranslatedSQL());
 	}
-	*/
+	
 
-	/*public Command getCommand(String sql) {
+	public Command getCommand(String sql) {
 		
-		 * CommandBuilder builder = new CommandBuilder(cassandraMetadata());
-		 * return builder.getCommand(sql);
+		  CommandBuilder builder = new CommandBuilder(solrMetadata());
+		  return builder.getCommand(sql);
 		
-	} */
+	} 
 
 	@Test
 	public void testSelect() throws Exception {
-		/*
-		 * testTranslation("select id from Person", "SELECT id FROM Person");
-		 * testTranslation("select name,age from Person",
-		 * "SELECT name, age FROM Person");
-		 * testTranslation("select * from Person", "SELECT * FROM Person");
-		 * testTranslation("select count(*) from Person limit 10",
-		 * "SELECT COUNT(*) FROM Person LIMIT 10"); testTranslation(
-		 * "select * from Person where id=1 and age>=18 and age<=100",
-		 * "SELECT * FROM Person WHERE id = 1 AND age >= 18 AND age <= 100");
-		 * testTranslation("select * from Person where id in(1,2,3)",
-		 * "SELECT * FROM Person WHERE id IN (1, 2, 3)");
-		 */
+		
+		  testTranslation("select * from example", "SELECT price, weight, popularity FROM example");
+//		  testTranslation("select name,age from Person",
+//		  "SELECT name, age FROM Person");
+//		  testTranslation("select * from Person", "SELECT * FROM Person");
+//		  testTranslation("select count(*) from Person limit 10",
+//		  "SELECT COUNT(*) FROM Person LIMIT 10"); testTranslation(
+//		  "select * from Person where id=1 and age>=18 and age<=100",
+//		 "SELECT * FROM Person WHERE id = 1 AND age >= 18 AND age <= 100");
+//		  testTranslation("select * from Person where id in(1,2,3)",
+//		  "SELECT * FROM Person WHERE id IN (1, 2, 3)");
+		 
 	}
 }
